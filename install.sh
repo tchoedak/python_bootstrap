47866859773a9ac79727bd477006e7123f85ab6b
tchoedak <tchoedak@gmail.com>
2018-08-15 00:08:30 -0700

    update, and create cleanup script

--- install.sh
+++ install.sh
@@ -1,13 +1,21 @@
-#/usr/bin/sh
-export PYTHON_VERSION=Python-2.7.15
-export PYTHON_INSTALL_DIR=resources/$PYTHON_VERSION
-wget \
-	--no-check-certificate\
-	-O $PYTHON_INSTALL_DIR\
-	https://www.python.org/ftp/python/$PYTHON_VERSION/$PYTHON_VERSION.tgz
-cd resources && tar -zxvf $PYTHON_VERSION.tgz
-cd $PYTHON_VERSION && ./configure && ./make
-export PYTHONPATH=$PYTHON_INSTALL_DIR
+#/bin/sh
+export ROOT_DIR=$PWD
+export VERSION=2.7.15
+export PYTHON_VERSION=Python-$VERSION # do not change this
+export PYTHON_INSTALL_DIR=$ROOT_DIR/resources/$PYTHON_VERSION
+export TEMP_DIR=$ROOT_DIR/resources/tmp
+wget -N \
+     --no-check-certificate\
+     -O $PYTHON_INSTALL_DIR.tgz\
+     https://www.python.org/ftp/python/$VERSION/$PYTHON_VERSION.tgz
+
+mkdir -p $TEMP_DIR
+tar -xzf $PYTHON_INSTALL_DIR.tgz -C $TEMP_DIR
+mv $TEMP_DIR/$PYTHON_VERSION $PYTHON_INSTALL_DIR
+$PYTHON_INSTALL_DIR/configure
+$PYTHON_INSTALL_DIR/make install
+exit 0
+export PYTHONPATH=$PYTHON_INSTALL_DIR/bin/
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
 pip install virtualenv
